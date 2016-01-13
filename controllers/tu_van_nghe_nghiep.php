e6ebab78112d9bff0af5d1768bb958c1d20d66e8
mmal93 <ducmonmz@gmail.com>
2016-01-13 21:44:01 +0700

    tu_van_nghe_nghiep haly

--- controllers/tu_van_nghe_nghiep.php
+++ controllers/tu_van_nghe_nghiep.php
@@ -11,7 +11,11 @@ class Tu_van_nghe_nghiep extends Controller {
 		$this->view->show_header_banner();
 		$this->view->showHeader();
 		$this->view->mainStart();
-		$data = $this->model->getJobs();
+		$data = $this->model->getData();
+		if($data) {
+			$this->view->assign('_new_data', $data);
+			$this->view->showTemplate('tu_van_nghe_nghiep'.DS.'new_unit');
+		}
 		$this->view->showPartner();
 		$this->view->mainEnd();
 		$this->view->showFooter();
